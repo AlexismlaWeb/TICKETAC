72d9e587bf755d4aa59e4f2bd67e8c2b10e4674c
stonoli <stonolidm@gmail.com>
2022-04-14 09:52:57 +0200

    bdd

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var options = {
  };
 
 // --------------------- BDD -----------------------------------------------------
-mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
+mongoose.connect('mongodb+srv://dev:dev@cluster0.pr7be.mongodb.net/TICKETAC?retryWrites=true&w=majority',
    options,
    function(err) {
     if (err) {
